24eea770f3306f5d3914d92e2e63c81981e06be8
Gatik Alagh <156594667+Gatik4321@users.noreply.github.com>
2024-07-24 05:21:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('Welcome to spotify');
+//Intialize the variables
+let SongIndex=0;
+let audioElement=new Audio('1.mp1');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('MyProgressBar');
+let songs=[
+    {
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+        songName:"Salam-e-Ishq",filePath:"songs/1.mp3",coverPath:"covers/1.jpg",
+    }
+]                                                                             
+//Writing the javascript code for my clone 
+audioElement.play();
+
+
+//Listen to Events
